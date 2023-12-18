84dc7fae4fb9542067af92183ebdfb7742ced551
SrRiuz <mateo.jimenez@alternova.com>
2023-12-18 09:15:27 -0500

    Adding new feature

--- src/data.js
+++ src/data.js
@@ -1,13 +1,20 @@
 
 import vivladiAppIcon from './assets/apps/vivaldi.webp'
+import trak from './assets/apps/trak.png'
+
 
 
 const PROJECT_DATA = [
     {
-        title:'Vivaldi math',
-        self:'https://play.google.com/store/apps/details?id=com.sririuz.vivaldi.calculator',
-        description:'Solve almost any math problem step by step! Equations, integrals, derivatives, limits and much more.',
-        icon:vivladiAppIcon
+        title: 'Vivaldi math',
+        self: 'https://play.google.com/store/apps/details?id=com.sririuz.vivaldi.calculator',
+        description: 'Solve almost any math problem step by step! Equations, integrals, derivatives, limits and much more.',
+        icon: vivladiAppIcon
+    }, {
+        title: 'Trak.',
+        self: 'https://play.google.com/store/apps/details?id=com.srriuz.app.trak.releace.aplication',
+        description: 'Discover great discounts in our app for affordable online shopping from top stores like Amazon, Linio, MercadoLibre, and eBay. Trak compares prices across platforms, ensuring you find what you need without overspending.',
+        icon: trak
     }
 ]
 
--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -9,37 +9,39 @@ import PROJECT_DATA from '../data'
 
 class Project extends React.Component {
 
-	constructor(props){
+	constructor(props) {
 		super(props)
 		this.state = {
-			hover:false
+			hover: false
 		}
 	}
 
-	onMouseEnter = (e,index) => {
-		this.setState({ hover:true,index:index })
+	onMouseEnter = (e, index) => {
+		this.setState({ hover: true, index: index })
 	}
 
 	onMouseLeave = (e) => {
-		this.setState({ hover:true,index:null })
+		this.setState({ hover: true, index: null })
 	}
 
-	render(){
+	render() {
 
 		document.title = 'Riuz web | Projects'
 
-		var projectItems = PROJECT_DATA.PROJECT_DATA.map((x,k) => {
+		var projectItems = PROJECT_DATA.PROJECT_DATA.map((x, k) => {
 			var hoverMode = null
-			if(this.state.index === k){
+			if (this.state.index === k) {
 				hoverMode = (
 					<div className={'hover-view'}>
 						<div className={'project-data-container'}>
-							<div style={{ width:'100%',paddingLeft:'70px' }}><strong id={'project-title'}>{x.title}</strong></div>
-							<div style={{ width:'100%',paddingLeft:'70px' }}><p>{x.description}</p></div>
-							<div style={{ width:'100%',marginBottom:'30px',paddingLeft:'70px' }}><strong style={{ fontWeight:'300',fontSize:'11px',color:'#909090' }}>More than 1k downloads</strong></div>
+							<div style={{ width: '100%', paddingLeft: '70px' }}><strong id={'project-title'}>{x.title}</strong></div>
+							<div style={{ width: '100%', paddingLeft: '70px' }}><p>{x.description}</p></div>
+							<div style={{ width: '100%', marginBottom: '30px', paddingLeft: '70px' }}>
+								<strong style={{ fontWeight: '300', fontSize: '11px', color: '#909090' }}>More than 1k downloads</strong>
+							</div>
 							<div>
 								<Button
-								    width={'20px'}
+									width={'20px'}
 									text='View in the Google play'
 									fontSize={'14px'}
 									onClick={() => {
@@ -54,28 +56,28 @@ class Project extends React.Component {
 
 			return (
 				<div
-				  index={k}
+					index={k}
 					key={k}
 					className={'project-card'}
-					onMouseEnter={(e) => {this.onMouseEnter(e,k)}}
+					onMouseEnter={(e) => { this.onMouseEnter(e, k) }}
 					onMouseLeave={this.onMouseLeave}
 				>
-					<div 
+					<div
 						className={'app-icon'}
-						style={{ backgroundImage:`url('${x.icon}')` }}
+						style={{ backgroundImage: `url('${x.icon}')` }}
 					></div>
-					{ hoverMode }
+					{hoverMode}
 				</div>
 			)
 		})
 
-		return(
+		return (
 			<div className={'project-screen'}>
 				<StackGrid
-				  columnWidth={385}
+					columnWidth={385}
 					gutterHeight={40}
 				>
-					{ projectItems }
+					{projectItems}
 				</StackGrid>
 			</div>
 		)
